5be64f9bc93a8def6484bf5b1d8255ec4b1a82e6
seth <seth.j.near@gmail.com>
2021-03-11 14:48:14 -0600

    commit tree e75aba155c5380a70225b49d02986295336b9a93

--- src/components/Band.js
+++ src/components/Band.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
 
+
 class Band extends Component {
+
+  handleOnClick = () => {
+    this.props.deleteBand(this.props.band.id)
+  }
+  
 
   render() {
     return(
       <div>
-        Band Component
+        <li>{this.props.band.name}</li><button onClick={this.handleOnClick}>DELETE</button>
       </div>
     );
   }
 };
 
 export default Band;
+
+
+// import React from 'react'
+
+// const Band = props => {
+//     return (
+//         <div>
+//            <li>{props.band.name}</li><button onClick={() => props.deleteBand(props.band.id)}>DELETE</button>
+//       </div>
+//     )
+// }
+// export default Band;
